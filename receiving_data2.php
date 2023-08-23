6f6422266f842f46ba019102247d871192cf4fb6
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:31:14 +0530

    Create receiving_data2.php

---
+++ receiving_data2.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1>Receiving data</h1>
+<?php
+$link=$_REQUEST["type"];
+$choice1=$_REQUEST["choice1"];
+$choice2=$_REQUEST["choice2"];
+?>
+<h2>Favourite <?php echo"$link"?>
+<ol>
+<li><?php echo"$choice1"?></li>
+<li><?php echo"$choice2"?></li>
+</body>
+</html>
