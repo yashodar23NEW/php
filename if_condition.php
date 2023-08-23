519d5a5a34fdc88fde43ae016f5f44619da947da
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:26:04 +0530

    Create if_condition.php

---
+++ if_condition.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1>using if condition</h1>
+<?php
+$marks=68;
+if($marks>=50)
+{
+echo "<h2><font color=' green',size='15'>You got $marks</font></h2>";}
+else
+{
+echo"You failed";}
+?>
+</body>
+</html>
