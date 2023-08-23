9e90d606d524719d871d86e5b1584a2e4aeb0958
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:20:37 +0530

    Create array_variable.php

---
+++ array_variable.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1>PHP with arrays and variable</h1>
+<?php
+$colors=array("fire"=>"red","sea"=>"blue");
+$col=$colors["sea"];
+echo "<h2> <center><font color='red'>the sky is  $col</font></center></h2>";
+?>
+</body>
+</html>
