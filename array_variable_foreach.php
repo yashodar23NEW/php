a761c7a6ceb8e46a447d4e03737e2c48aabff28f
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:22:12 +0530

    Create array_variable_foreach.php

---
+++ array_variable_foreach.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1>PHP with arrays and variable</h1>
+<?php
+$colors=array("blue","red","yellow","green");
+foreach($colors as $col)
+echo "<h2> <font color='red'><li>I like $col</li></font></h2>";
+?>
+</body>
+</html>
