5287dd37bae541272f1d33a9977a70a75affdf10
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:29:22 +0530

    Create receiving_data.php

---
+++ receiving_data.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>Receiving data</h1>
+<?php
+$uid=$_REQUEST["userid"];
+$uname=$_REQUEST["username"];
+$status=$_REQUEST["status"];
+echo " User id =$uid<br>";
+echo"user name=$uname<br>";
+echo "Status =$status";
+?>
+</body>
+</html>
