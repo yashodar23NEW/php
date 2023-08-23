60b0fd279248fa08d5e4ceb239c30d7e6f8cec76
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:09:30 +0530

    Create add_comment.php

---
+++ add_comment.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>My first PHP page</h1>
+<?php
+// print Hello World
+echo "<h2><center><font color='Blue'face='Arial'>Hello World</font></center></h2>";
+?>
+</body>
+</html>
+
+
