2a88df4fff04cd74040b701c4983928e0e8ec05c
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:26:59 +0530

    Create sending_data.php

---
+++ sending_data.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>Sending Data to the Server</h1>
+<hr>
+<a href = "http://localhost/data1.php?userid=1001&username=abc&status=off"> Click here to send the data
+</a>
+</body>
+</html>
