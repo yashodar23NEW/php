cae6227e03ed268be2055e6012800470f76c2454
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2023-08-23 12:46:57 +0530

    Create get_fileinformation.php

---
+++ get_fileinformation.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2>File information</h2>
+<?php
+$fname=$_Files["file1"]["name"];
+$type=$_Files["file1"]["type"];
+$size=$_Files["file1"]["size"];
+echo"File name is :$fname<br>";
+echo"File type is:$type<br>";
+echo"File size is: $size";
+?>
+</body>
+</html>
